73f3eb59c2afdcf529d26669f598ab56f3557c9c
Dmitriy Sannikov <omeom@mail.ru>
2024-07-09 12:34:00 +0400

    Add tick tack toe

---
+++ ConsoleGame/TickTackToe/TickTackToe/MyTickTackGame.cs
@@ -0,0 +1,84 @@
+﻿namespace tick_tack_toe
+{
+
+    public enum StateTabel
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+
+    public enum GameWinner
+    {
+        Crosses,
+        Zeros,
+        Unfinished,
+        Draw
+    }
+    internal class MyTickTackGame
+    {
+        private const int BoardSize = 9;
+        private readonly StateTabel[] Fields = new StateTabel[BoardSize];
+        public int MoveCounter { get; private set; }
+
+
+        public MyTickTackGame()
+        {
+            for (int i = 0; i < Fields.Length; i++)
+            {
+                Fields[i] = StateTabel.Unset;
+            }
+        }
+
+        public void MakeMove(int index)
+        {
+            Fields[index - 1] = MoveCounter % 2 == 0 ? StateTabel.Cross : StateTabel.Zero;
+            MoveCounter++;
+        }
+
+        public StateTabel GetState(int index)
+        {
+            return Fields[index - 1];
+        }
+
+
+        private static readonly int[][] WinningCombinations = new int[][]
+        {
+            new int[] { 1, 2, 3 },
+            new int[] { 4, 5, 6 },
+            new int[] { 7, 8, 9 },
+            new int[] { 1, 4, 7 },
+            new int[] { 2, 5, 8 },
+            new int[] { 3, 6, 9 },
+            new int[] { 1, 5, 9 },
+            new int[] { 3, 5, 7 },
+        };
+
+        public GameWinner GetWinner()
+        {
+            foreach (var combination in WinningCombinations)
+            {
+                if (AsSame(combination[0], combination[1], combination[2]))
+                {
+                    StateTabel state = GetState(combination[0]);
+                    if (state != StateTabel.Unset)
+                    {
+                        return state == StateTabel.Cross ? GameWinner.Crosses : GameWinner.Zeros;
+                    }
+                }
+            }
+            return MoveCounter < BoardSize ? GameWinner.Unfinished : GameWinner.Draw;
+        }
+
+
+        private bool AsSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+        }
+
+
+
+
+
+    }
+}
---
+++ ConsoleGame/TickTackToe/TickTackToe/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+
+namespace tick_tack_toe
+{
+    internal class Program
+    {
+        public static MyTickTackGame game = new MyTickTackGame();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This is a tick tack toe");
+
+            Console.WriteLine(GetPrintableState());
+            while (game.GetWinner() == GameWinner.Unfinished)
+            {
+                int index;
+                while (!int.TryParse(Console.ReadLine(), out index) || index < 1 || index > 9
+                        || game.GetState(index) != StateTabel.Unset)
+                {
+                    Console.WriteLine("Incorrect inpup. Please enter a number again");
+                }
+                game.MakeMove(index);
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
+            }
+
+            Console.WriteLine($"Winner: {game.GetWinner()}");
+            Console.ReadLine();
+
+
+        }
+        private static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 1; i <= 7; i += 3)
+            {
+                sb.AppendLine("     |     |     |")
+                    .AppendLine(
+                    $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |  ")
+                    .AppendLine("_____|_____|_____|");
+            }
+            return sb.ToString();
+        }
+
+        static string GetPrintableChar(int i)
+        {
+            StateTabel state = game.GetState(i);
+            if (state == StateTabel.Unset)
+                return i.ToString();
+            return state == StateTabel.Cross ? "X" : "O";
+        }
+
+    }
+}
